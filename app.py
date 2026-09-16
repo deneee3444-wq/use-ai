074cebb6d7d2fdc05b3e06b1783e0d3a7a2837b2
deneee3444-wq <deneee3444@gmail.com>
2026-09-16 20:38:34 +0300

    Update app.py

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ UA = (
 APP_PASSWORD = "123"
 
 PROXY_URL = os.environ.get(
-    "PROXY_URL", "http://plasthjw-1:vh917rfhzcxi@p.webshare.io:80"
+    "PROXY_URL", "http://plasthjw-2:vh917rfhzcxi@p.webshare.io:80"
 )
 
 
